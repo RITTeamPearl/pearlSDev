059825b7a31939391e4c671a83c9ac84c044be3a
Gavin Drabik <grd2747@g.rit.edu>
2018-09-04 11:26:45 -0400

    small folder structure change

--- views/login.php
+++ views/login.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once '../data_layer.php';
+    require_once '../database/data_layer.php';
     $dataLayer = new data_layer();
     //$connection = mysqli_connect("localhost", "root", "student", "rrcc_pearl_db") or die (mysqli_error()); //Connect to server
     //$email = mysqli_real_escape_string($connection, $_POST['email']);
