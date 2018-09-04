6932558f5ff4edf7e8ea14b32574f82e6d39d462
Gavin Drabik <grd2747@g.rit.edu>
2018-09-04 11:23:18 -0400

    Database folder added
    database folder to hold data layer php file and DB script

---
+++ database/dbDump.sql
@@ -0,0 +1,164 @@
+-- MySQL dump 10.14  Distrib 5.5.56-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: rrcc_pearl_db
+-- ------------------------------------------------------
+-- Server version	5.5.56-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acknowledgement`
+--
+
+DROP TABLE IF EXISTS `acknowledgement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acknowledgement` (
+  `notificationID` int(15) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `viewedYN` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`notificationID`,`phone`),
+  KEY `ack_phone_fk_idx` (`phone`),
+  CONSTRAINT `ackNotiID_notiNotiID` FOREIGN KEY (`notificationID`) REFERENCES `notification` (`notificationID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `ack_phone_fk` FOREIGN KEY (`phone`) REFERENCES `user` (`phone`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acknowledgement`
+--
+
+LOCK TABLES `acknowledgement` WRITE;
+/*!40000 ALTER TABLE `acknowledgement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acknowledgement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authorization`
+--
+
+DROP TABLE IF EXISTS `authorization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authorization` (
+  `authID` int(2) NOT NULL,
+  `authName` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`authID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authorization`
+--
+
+LOCK TABLES `authorization` WRITE;
+/*!40000 ALTER TABLE `authorization` DISABLE KEYS */;
+INSERT INTO `authorization` VALUES (1,'unauthorized'),(2,'employee'),(3,'deptHead'),(4,'admin');
+/*!40000 ALTER TABLE `authorization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `deptID` int(2) NOT NULL,
+  `deptName` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`deptID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1,'HR'),(2,'admin'),(3,'sales'),(4,'production'),(5,'operations'),(6,'foodAndBev'),(7,'garage');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notification` (
+  `notificationID` int(15) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `body` varchar(2500) DEFAULT NULL,
+  `attachment` varchar(45) DEFAULT NULL,
+  `activeYN` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`notificationID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notification`
+--
+
+LOCK TABLES `notification` WRITE;
+/*!40000 ALTER TABLE `notification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `phone` varchar(11) NOT NULL,
+  `fName` varchar(26) DEFAULT NULL,
+  `lName` varchar(26) DEFAULT NULL,
+  `activeYN` tinyint(4) DEFAULT NULL,
+  `temporaryYN` tinyint(4) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `carrier` varchar(25) DEFAULT NULL,
+  `deptID` int(2) DEFAULT NULL,
+  `authID` int(2) DEFAULT NULL,
+  PRIMARY KEY (`phone`),
+  KEY `user_dept_fk_idx` (`deptID`),
+  KEY `user_auth_fk_idx` (`authID`),
+  CONSTRAINT `user_auth_fk` FOREIGN KEY (`authID`) REFERENCES `authorization` (`authID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_dept_fk` FOREIGN KEY (`deptID`) REFERENCES `department` (`deptID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ("5856455810",'Testy','McAdmin',1,0,'password','email@domain.com','verizon',1,4);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-30 14:06:01
