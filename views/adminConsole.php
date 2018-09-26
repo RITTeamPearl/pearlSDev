ec72c39b4093b91e886df662c8fb39cc9f5e0874
Mason Santora <>
2018-09-25 23:31:54 -0400

    adminConsole.php - Added row duplicate - Added icon for new notification - Added table row spacer & styling - Styling for multiple rows - Styling for pagination

--- views/adminConsole.php
+++ views/adminConsole.php
@@ -55,41 +55,70 @@
                 <td><i class='fas fa-pencil-alt'></i></td>
                 <td><i class='fas fa-trash-alt'></i></td>
             </tr>
+            
+            <!-- Spacer puts padding in-between table rows -->
+            
+            <tr class='spacer'><td></td></tr>
+            
+            <tr class='collapsed'>
+                <td><i class='fas fa-chevron-circle-down'></i></td> <!-- Onclick this icon needs to be updated to fas fa-chevron-circle-up -->
+                <td>Heavy Rain to delay bla bla bla bla bla</td>
+                <td>Yes</td>
+                <td><i class='fas fa-pencil-alt'></i></td>
+                <td><i class='fas fa-trash-alt'></i></td>
+            </tr>
+            
+            <tr class='spacer'><td></td></tr>
+            
+            <!-- Row that is hidden in collapsed row, needs JS to unhide this https://codepen.io/andornagy/pen/gaGBZz -->
+
+            <tr>
+                <td colspan='5' class='un-collapsed'>
+                <h2>Body</h2>
+                <p>Lorem ipsum dolor sit amet, consecteur adiposing elit. Sed autor ligula quis ante pretium lacreet.Nuno semper erat dignissim placerate feugiat.
+                Aenean commodo risus consequeat ligula aliquet portior. Proin turpis vitae commodo mattis, massa felis accumsan.</p>
+
+                <h2>Attachment</h2>
+                <p>document.pdf</p><i></i>
+
+                <h2>User Acknowledgements Report</h2>
+                <p>user_report.csv</p><i></i>
+                </td>
+            </tr>
+
+            <tr class='spacer'><td></td></tr>
+            
+            <!-- Begin next dynamically added rows here -->
+
+            <!-- Add New Notification -->
+            <tr class='collapsed'>
+                <td><i class='fas fa-plus-circle'></i></td>
+                <td colspan='4'>Add New Notification</td>
+            </tr>
 
         </table>
-
-        <!-- Row that is hidden in collapsed row, needs JS to unhide this https://codepen.io/andornagy/pen/gaGBZz -->
-
-        <tr>
-            <h2>Body</h2>
-            <p>Lorem ipsum dolor sit amet, consecteur adiposing elit. Sed autor ligula quis ante pretium lacreet.Nuno semper erat dignissim placerate feugiat.
-            Aenean commodo risus consequeat ligula aliquet portior. Proin turpis vitae commodo mattis, massa felis accumsan.</p>
-
-            <h2>Attachment</h2>
-            <p>document.pdf</p><i></i>
-
-            <h2>User Acknowledgements Report</h2>
-            <p>user_report.csv</p><i></i>
-
-        </tr>
-
-        <!-- Begin next dynamically added rows here -->
-
-        <!-- Add New Notification -->
-        <tr>
-            <td><i></i></td>
-            <td colspan='4'>Add New Notification</td>
-        </tr>
 
         <!-- Pagination
         1. Counts Entries on screen (max 5, but could be less)
         2. Count Total Number of Entries
         3. Shows previous page of entries, disable when none to show
         4. Shows next page of entries, disable when none to show
+        5. If 'Back' or 'Next' is clickable add 'clickable' class to it
         -->
-        <p>1-5 of 13</p>
-        <i><p>Back</p></i>
-        <i><p>Next</p></i>
+        <div class='pagination block'>
+            <div class='number inline'>
+                <span>1-5 of 13</span>
+            </div>
+            
+            <div class='back inline'>
+                <i class='fas fa-chevron-left'></i><span>Back</span>
+            </div>
+            
+            <div class='next inline'>
+                <span>Next</span><i class='fas fa-chevron-right'></i>
+            </div>
+        </div>
+        
     </section>
         
 </body>
