e6733ddf7e75cc3e12e8300ae13eea3166b92aa1
datoria <dh2603@rit.edu>
2018-10-09 16:10:07 -0400

    Removed unnecessary files

---
+++ business_layer.php
@@ -0,0 +1,144 @@
+<?php
+require '../vendor/autoload.php';
+//require '../database/data_layer.php';
+use Twilio\Rest\Client;
+use PHPMailer\PHPMailer\PHPMailer;
+class business_layer{
+
+    function sendEmail($address,$subject, $body){
+        //need to set time for smtp purposes
+        date_default_timezone_set('America/New_York');
+
+        //new instance, set to google smtp settings standards
+        $mail = new PHPMailer(TRUE);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+
+        //email address to send from
+        $mail->Username = $_SERVER['EMAIL_ADDR'];
+        //password
+        $mail->Password = $_SERVER['EMAIL_PASSWORD'];
+
+        try{
+            //set attributes to passed in
+            $mail->setFrom('team-pearl-rit@gmail.com', 'RIT Team Pearl');
+            $mail->addAddress($address);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->IsHTML(true);
+
+            //Send it
+            $mail->send();
+            return true;
+        }
+        catch (\Exception $e)//Slash is for generic exception class
+        {
+           echo $e->getMessage();
+           return false;
+        }
+    }
+
+    function sendText(){
+        // Twilio credentials
+        $account_sid = $_SERVER['TWILIO_SID'];
+        $auth_token = $_SERVER['TWILIO_TOKEN'];
+        $twilio_number = $_SERVER['TWILIO_PHONE'];;
+
+        //Create twilio client using our credentials
+        $client = new Client($account_sid, $auth_token);
+        //send message
+        $client->messages->create(
+            // Where to send a text message (your cell phone?)
+            '+15856455810',//should be a passed in $phoneNumber but free version doesnt allow it.
+            array(
+                'from' => $twilio_number,
+                'body' => 'It Works!'
+            )
+        );
+    }
+
+    function passwordReset($email){
+        $hashedEmail = password_hash($email,PASSWORD_DEFAULT);
+
+        $subject = "RRCC Account Password Reset";
+        $body = "<h1>Please click the link below to reset your password</h1>";
+        $body .= "<h2 href='localhost/views/resetConfirm.php?email=$hashedEmail' >Reset</h2>";
+        //generate url to send in email. ../resetPassword.php?emailToReset
+        //send email
+        //confirm email that is being reset
+        //if input matches $_GET as well as some unique identifier then reset the password
+
+
+        // $dataLayer = new data_layer();
+        // //generate a random 10 character string.
+        // $genPass = substr(md5(microtime()),rand(0,26),10);
+        // //database call to set new password and update temp password flag
+        // $dataLayer->setUserTempPass($email,$genPass);
+        //echo "your new password is $genPass";
+
+        //address , subject line, body
+        //if($bizLayer->sendEmail($_POST['email'], 'Password Reset test', "Your New Password is $genPass")){
+        //};
+    }
+
+    function validateAndSanitize($postData){
+
+        //$validatedPOST = array();
+        //$validatedPOST['phone'] = $postData['phone'];// this should be validated and sanitized
+        //return $validatedPOST;
+    }
+
+    function createNewsTable($notificationArray){
+        $string = "";
+        $rowCount = 1;
+        $nextRowCount = 2;
+        foreach ($notificationArray as $rowArray) {
+            //var_dump($rowArray);
+            $currNotiID = $rowArray['notificationID'];
+            $currTitle = $rowArray['title'];
+            $currBody = $rowArray['body'];
+            $currAttachment = $rowArray['attachment'];
+            $currActiveYN = ($rowArray['active']) ? ('yes') : ('no');
+
+            $string .= <<<END
+            <tr id = "row-{$rowCount}"class='collapsed'>
+                <td><i onclick="dropDownToggle(this)" class='fas fa-chevron-circle-down'></i></td> <!-- Onclick this icon needs to be updated to fas fa-chevron-circle-up -->
+                <td>{$currTitle}</td>
+                <td>{$currActiveYN}</td>
+                <td><i onclick="dropDownModify(this);" class='fas fa-pencil-alt'></i></td>
+                <td><form action="delete.php?id={$currNotiID}" method="post"> <button style="background-color: transparent; border-color:transparent" type="submit"><i class='fas fa-trash-alt'></i></button></form></td>
+            </tr>
+
+            <tr class='spacer'><td></td></tr>
+
+            <!-- Row that is hidden in collapsed row, needs JS to unhide this https://codepen.io/andornagy/pen/gaGBZz -->
+            <!-- JQUERY Animate function does not work on TR so eventually we might want to convert this to a ul? -->
+            <tr id = "row-{$nextRowCount}" class='un-collapsed'>
+                <td colspan='3' class='leftUnCollapsed'>
+                    <h2>Body</h2>
+                    <span>{$currBody}</span>
+                </td>
+                <td colspan='2' class='rightUnCollapsed'>
+                    <h2>Attachment</h2>
+                    <!-- Make this 'fas fa-file-upload' with blue color, if no file exists and text saying 'No attachment' Create functionality for upload -->
+                    <i class="fas fa-times-circle"></i><span>{$currAttachment}</span>
+
+                    <h2>User Ack. Report</h2>
+                    <i class="fas fa-download"></i><span>user_report.csv</span>
+                </td>
+            </tr>
+            <tr class='spacer'><td></td></tr>
+END;
+        $rowCount++;
+        $nextRowCount++;
+        }
+
+        return $string;
+        //var_dump($notificationArray);
+    }
+
+
+}
---
+++ createAcct.php
@@ -0,0 +1,122 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST){
+    require_once '../database/data_layer.php';
+    $dataLayer = new data_layer();
+
+    if (count($_POST) == 7) {
+        //business layer validation / sanitization of $_POST
+        $dataLayer->createNewUser($_POST);
+    }
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rochester Riverside Convention Center</title>
+    <meta charset='utf-8'/>
+    <meta name='viewport' content='width=device-width, initial-scale = 1.0, minimum-scale = 1.0, maximum-scale = 5.0' />
+    <link rel='stylesheet' type='text/css' media='screen' href='/style/css/createAcct.css'>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script type='text/javascript' src='/js/scripts.js'></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <link href='../assets/fonts/fontawesome-free-5.2.0-web/css/all.min.css' rel='stylesheet'>
+</head>
+
+<body onload="addMask()" id='createAcctPage' class='backgroundImage'>
+    <div class='container'>
+        <!-- Landing Section -->
+        <section id='create-form'>
+            <!-- Header -->
+            <div class='header'>
+                <h1 class='title centered'>Create Account</h1>
+                <h2 class='subtitle centered'>We just need a little information for the<br/>HR department to activate your account.</h2>
+            </div>
+
+            <!-- Screen Identifier Visual Graphic -->
+            <div id='screenIdentifier' class='block centered'>
+                <div id='screenContainer'>
+                    <div class='dotCont' id='dotCont1'>
+                        <i class='fas fa-circle inline' id='dot1'></i>
+                        <i class='far fa-dot-circle inline' id='circle1'></i>
+                        <hr class='whiteLine inline'>
+                    </div>
+
+                    <div class='dotCont' id='dotCont2'>
+                        <i class='fas fa-circle inline' id='dot2'></i>
+                        <i class='far fa-dot-circle inline' id='circle2'></i>
+                        <hr class='whiteLine inline'>
+                    </div>
+
+                    <div class='dotCont' id='dotCont3'>
+                        <i class='fas fa-circle inline' id='dot3'></i>
+                        <i class='far fa-dot-circle inline' id='circle3'></i>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Send Password Form -->
+            <form class='formContainer' action='createAcct.php' method='POST'>
+
+                <!-- Create Screen 1. Outer div is for JS -->
+                <div class="formStep" id="formStep1">
+                    <div class='inputWithIcon'>
+                        <input class='block' id='phoneNumber' type = 'text' placeholder= 'Phone Number' name='phoneNumber' required="required">
+                        <i class='fas fa-phone' aria-hidden='true'></i>
+                    </div>
+
+                    <div class='inputWithIcon'>
+                        <input onkeyup="confirmPassword()" class='block' id='password' type = 'password' placeholder= 'Password' name='password' required="required">
+                        <i class='fas fa-key pwIcon' aria-hidden='true'></i>
+                    </div>
+
+                    <div class='inputWithIcon'>
+                        <input onkeyup="confirmPassword()" class='block' id='passwordConfirm' type = 'password' placeholder= 'Confirm Password' name='passwordConfirm' required="required">
+                        <i class='fas fa-lock pwIcon' aria-hidden='true'></i>
+                    </div>
+                    <input class='block submit centered' id='next' type = 'button' value= 'Continue' onclick="nextStep(1)"/>
+                    <a id='login' href='../index.php'>Log In</a>
+                </div>
+
+                <!-- Create Screen 2 -->
+                <div class="formStep" id="formStep2" style="display: none;">
+                    <div class='inputWithIcon'>
+                        <input class='block' id='fName' type = 'text' placeholder= 'First Name' name='fName' required="required" autofocus>
+                        <i class='fas fa-address-card' aria-hidden='true'></i>
+                    </div>
+                    <div class='inputWithIcon'>
+                        <input class='block' id='lName' type = 'text' placeholder= 'Last Name' name='lName' required="required" autofocus>
+                        <i class='fas fa-address-card' aria-hidden='true'></i>
+                    </div>
+                    <input class='block submit centered' id='next' type = 'button' value= 'Continue' onclick="nextStep(2)"/>
+                    <a id='login' href='../index.php'>Log In</a>
+                </div>
+
+                <div class="formStep" id="formStep3" style="display: none;">
+                    <div class='inputWithIcon'>
+                        <input class='block' id='email' type = 'text' placeholder= 'E-mail Address' name='email' required="required" autofocus>
+                        <i class='fas fa-user' aria-hidden='true'></i>
+                    </div>
+
+                    <div class='inputWithIcon'>
+                        <select class='block inputWithIcon' id='dept' name='dept' required="required">
+                            <option value="" disabled selected>Department</option>
+                            <option value="1">HR</option>
+                            <option value="2">Admin</option>
+                            <option value="3">Sales</option>
+                            <option value="4">Production</option>
+                            <option value="5">Operations</option>
+                            <option value="6">Food and Beverage</option>
+                            <option value="7">Garage</option>
+                        </select>
+                        <i class='fas fa-building' aria-hidden='true'></i>
+                    </div>
+                    <input class='block submit centered' id='submit' type = 'submit' value= 'Send Request'/>
+                    <a id='login' href='../index.php'>Log In</a>
+                </div>
+
+            </form>
+        </section>
+    </div>
+</body>
+</html>
