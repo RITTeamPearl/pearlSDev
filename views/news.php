586143299c524f340a0943c73d4ecc52fc00993f
Mason Santora <>
2018-10-22 12:17:35 -0400

    news - Built out and styled landing page (News Page)
    assets
    - Added some new assets for the landing page
    
    styling
    - Added news page styling sass/css

---
+++ views/news.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rochester Riverside Convention Center</title>
+    <meta charset='utf-8'/>
+    <meta name='viewport' content='width=device-width, initial-scale = 1.0, minimum-scale = 1.0, maximum-scale = 5.0' />
+    <link rel='stylesheet' type='text/css' media='screen' href='/style/css/news.css'>
+    <link href='../assets/fonts/fontawesome-free-5.2.0-web/css/all.min.css' rel='stylesheet'>
+</head>
+
+<body id='newsPage'>
+
+    <!-- News -->
+    <div class='bodyContainer'>
+        <div class='notifContainer' id='1'>
+            <div class='overlay'>
+                <img src='../assets/images/1.jpg'>
+            </div>
+
+            <h2 class='title'>Convention center to be closed on August 24, 2018</h2>
+
+            <div class='subtitle block'>
+                <div class='posted inline'>
+                    <i class="far fa-clock"></i>
+                    <span class='inline'>8h ago</span>
+                </div>
+                <a class='inline' href=''>read more</a>
+            </div>
+
+            <!-- Admin Feature only -->
+            <button type='button' class='button hidden'><i class="far fa-edit"></i></button>
+
+            <div class='buttonOptions hidden'>
+                <ul class='spaced'>
+                    <li>Modify<i class='fas fa-pencil-alt'></i></li>
+                    <li>Delete<i class="fas fa-trash-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class='notifContainer' id='2'>
+            <div class='overlay'>
+                <img src='../assets/images/2.jpg'>
+            </div>
+
+            <h2 class='title'>Heavy Rain to delay conference center opening</h2>
+
+            <div class='subtitle block'>
+                <div class='posted inline'>
+                    <i class="far fa-clock"></i>
+                    <span class='inline'>1w ago</span>
+                </div>
+                <a class='inline' href=''>read more</a>
+            </div>
+
+            <!-- Admin Feature only -->
+            <button type='button' class='button hidden'><i class="far fa-edit"></i></button>
+
+            <div class='buttonOptions hidden'>
+                <ul class='spaced'>
+                    <li>Modify<i class='fas fa-pencil-alt'></i></li>
+                    <li>Delete<i class="fas fa-trash-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <!-- Footer -->
+        <div class='footer block'>
+            <ul class='iconContainer'>
+                <li class='inline'><i class="fas fa-newspaper"></i></li>
+                <li class='inline'><i class="fas fa-video"></i></li>
+                <li class='inline'><i class="fas fa-user"></i></li>
+                <li class='inline'><i class="fas fa-bell"></i></li>
+                <li class='inline'><i class="fas fa-toolbox"></i></li>
+            </ul>
+
+        </div>
+    </div>
+</body>
+</html>
